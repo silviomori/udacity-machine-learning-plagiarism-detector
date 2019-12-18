511d0d5bda28ea99731dd4471564771e8406fb2b
Silvio Mori Neto <silvio@technomori.com>
2019-12-18 07:03:30 -0300

    feat: Build and train a custom PyTorch model
    Recall:     1.000
    Precision:  0.938
    Accuracy:   0.960

--- source_pytorch/model.py
+++ source_pytorch/model.py
@@ -1,44 +1,67 @@
-# torch imports
-import torch.nn.functional as F
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-## TODO: Complete this classifier
+## DONE: Complete this classifier
 class BinaryClassifier(nn.Module):
     """
     Define a neural network that performs binary classification.
     The network should accept your number of features as input, and produce 
     a single sigmoid value, that can be rounded to a label: 0 or 1, as output.
-    
+
     Notes on training:
     To train a binary classifier in PyTorch, use BCELoss.
-    BCELoss is binary cross entropy loss, documentation: https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
+    BCELoss is binary cross entropy loss, documentation:
+    https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
     """
 
-    ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
-    def __init__(self, input_features, hidden_dim, output_dim):
+    ## DONE: Define the init function, the input params are required
+    # (for loading code in train.py to work)
+    def __init__(self, input_features, hidden_dim, output_dim, dropout=0.2):
         """
         Initialize the model by setting up linear layers.
         Use the input parameters to help define the layers of your model.
-        :param input_features: the number of input features in your training/test data
-        :param hidden_dim: helps define the number of nodes in the hidden layer(s)
+
+        Arguments:
+        :param input_features: the number of input features in your \
+            training/test data
+        :param hidden_dim: define the number of nodes in the hidden layer(s)
         :param output_dim: the number of outputs you want to produce
+        :param dropout: probability of an element to be zeroed. Default: 0.2
         """
         super(BinaryClassifier, self).__init__()
 
-        # define any initial layers, here
-        
+        nodes = []
+        nodes.append(input_features)
+        if type(hidden_dim) == int:
+            nodes.append(hidden_dim)
+        elif type(hidden_dim) == list:
+            nodes.extend(hidden_dim)
+        nodes.append(output_dim)
 
-    
-    ## TODO: Define the feedforward behavior of the network
+        self.module_list = nn.ModuleList()
+        for n_in, n_out in zip(nodes[:-1], nodes[1:]):
+            self.module_list.append(nn.Linear(n_in, n_out))
+
+        self.dropout = nn.Dropout(dropout)
+
+    ## DONE: Define the feedforward behavior of the network
     def forward(self, x):
         """
         Perform a forward pass of our model on input features, x.
-        :param x: A batch of input features of size (batch_size, input_features)
+
+        Arguments:
+        :param x: A batch of input features of size (batch_size,input_features)
+
+        Return:
         :return: A single, sigmoid-activated value as output
         """
-        
+
         # define the feedforward behavior
-        
-        return x
-    
+        for layer in self.module_list[:-1]:
+            x = F.relu(layer(x))
+            x = self.dropout(x)
+        x = self.module_list[-1](x)
+
+        return torch.sigmoid(x)
--- source_pytorch/train.py
+++ source_pytorch/train.py
@@ -3,12 +3,14 @@ import json
 import os
 import pandas as pd
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
 
 # imports the model in model.py by name
 from model import BinaryClassifier
 
+                                                                               
 def model_fn(model_dir):
     """Load the PyTorch model from the `model_dir` directory."""
     print("Loading model.")
@@ -23,7 +25,9 @@ def model_fn(model_dir):
 
     # Determine the device and construct the model.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = BinaryClassifier(model_info['input_features'], model_info['hidden_dim'], model_info['output_dim'])
+    model = BinaryClassifier(model_info['input_features'],
+                             model_info['hidden_dim'],
+                             model_info['output_dim'])
 
     # Load the stored model parameters.
     model_path = os.path.join(model_dir, 'model.pth')
@@ -40,7 +44,8 @@ def model_fn(model_dir):
 def _get_train_data_loader(batch_size, training_dir):
     print("Get train data loader.")
 
-    train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
+    train_data = pd.read_csv(os.path.join(training_dir, "train.csv"),
+                             header=None, names=None)
 
     train_y = torch.from_numpy(train_data[[0]].values).float().squeeze()
     train_x = torch.from_numpy(train_data.drop([0], axis=1).values).float()
@@ -49,20 +54,20 @@ def _get_train_data_loader(batch_size, training_dir):
 
     return torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
 
-
 # Provided training function
 def train(model, train_loader, epochs, criterion, optimizer, device):
     """
-    This is the training method that is called by the PyTorch training script. The parameters
+    This is the training method that is called by the PyTorch training script.
+    The parameters
     passed are as follows:
     model        - The PyTorch model that we wish to train.
     train_loader - The PyTorch DataLoader that should be used during training.
     epochs       - The total number of epochs to train for.
-    criterion    - The loss function used for training. 
+    criterion    - The loss function used for training.
     optimizer    - The optimizer to use during training.
     device       - Where the model and data should be loaded (gpu or cpu).
     """
-    
+
     # training loop is provided
     for epoch in range(1, epochs + 1):
         model.train() # Make sure that the model is in training mode.
@@ -80,32 +85,36 @@ def train(model, train_loader, epochs, criterion, optimizer, device):
 
             # get predictions from model
             y_pred = model(batch_x)
-            
+
             # perform backprop
             loss = criterion(y_pred, batch_y)
             loss.backward()
             optimizer.step()
-            
+
             total_loss += loss.data.item()
 
-        print("Epoch: {}, Loss: {}".format(epoch, total_loss / len(train_loader)))
+        print("Epoch: {}, Loss: {}".
+              format(epoch, total_loss / len(train_loader)))
 
-
-## TODO: Complete the main code
+## DONE: Complete the main code
 if __name__ == '__main__':
     
     # All of the model parameters and training parameters are sent as arguments
     # when this script is executed, during a training job
-    
+
     # Here we set up an argument parser to easily access the parameters
     parser = argparse.ArgumentParser()
 
-    # SageMaker parameters, like the directories for training data and saving models; set automatically
+    # SageMaker parameters, like the directories for
+    # training data and saving models; set automatically
     # Do not need to change
-    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    
+    parser.add_argument('--output-data-dir', type=str,
+                        default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model-dir', type=str,
+                        default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str,
+                        default=os.environ['SM_CHANNEL_TRAINING'])
+
     # Training Parameters, given
     parser.add_argument('--batch-size', type=int, default=10, metavar='N',
                         help='input batch size for training (default: 10)')
@@ -113,11 +122,22 @@ if __name__ == '__main__':
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    
-    ## TODO: Add args for the three model parameters: input_features, hidden_dim, output_dim
+
+    ## DONE: Add args for the three model parameters:
+    # input_features, hidden_dim, output_dim
     # Model Parameters
-    
-    
+    parser.add_argument('--input-features', type=int, default=2, metavar='IN',
+                        help='number of input features')
+    parser.add_argument('--hidden-dim', type=list, metavar='H',
+                        nargs='+', default=[64,64],
+                        help='size of hidden layers')
+    parser.add_argument('--output-dim', type=int, default=1, metavar='OUT',
+                        help='number of outputs')
+    parser.add_argument('--dropout', type=float, default=0.2, metavar='D',
+                        help='probability of an element to be zeroed')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+
     # args holds all passed-in arguments
     args = parser.parse_args()
 
@@ -131,34 +151,39 @@ if __name__ == '__main__':
 
 
     ## --- Your code here --- ##
-    
-    ## TODO:  Build the model by passing in the input params
-    # To get params from the parser, call args.argument_name, ex. args.epochs or ards.hidden_dim
-    # Don't forget to move your model .to(device) to move to GPU , if appropriate
-    model = None
 
-    ## TODO: Define an optimizer and loss function for training
-    optimizer = None
-    criterion = None
+    ## DONE:  Build the model by passing in the input params
+    # To get params from the parser, call args.argument_name,
+    # eg. args.epochs or args.hidden_dim
+    # Don't forget to move your model .to(device)
+    # to move to GPU, if appropriate
+    model = BinaryClassifier(args.input_features, args.hidden_dim,
+                             args.output_dim, args.dropout)
+    model.to(device)
 
-    # Trains the model (given line of code, which calls the above training function)
+    ## DONE: Define an optimizer and loss function for training
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    criterion = nn.BCELoss()
+
+    # Trains the model
+    # (given line of code, which calls the above training function)
     train(model, train_loader, args.epochs, criterion, optimizer, device)
 
-    ## TODO: complete in the model_info by adding three argument names, the first is given
-    # Keep the keys of this dictionary as they are 
+    ## DONE: complete in the model_info by adding three argument names,
+    # Keep the keys of this dictionary as they are
     model_info_path = os.path.join(args.model_dir, 'model_info.pth')
     with open(model_info_path, 'wb') as f:
         model_info = {
             'input_features': args.input_features,
-            'hidden_dim': <add_arg>,
-            'output_dim': <add_arg>,
+            'hidden_dim': args.hidden_dim,
+            'output_dim': args.output_dim
         }
         torch.save(model_info, f)
-        
-    ## --- End of your code  --- ##
-    
 
-	# Save the model parameters
+    ## --- End of your code  --- ##
+
+
+    # Save the model parameters
     model_path = os.path.join(args.model_dir, 'model.pth')
     with open(model_path, 'wb') as f:
         torch.save(model.cpu().state_dict(), f)
